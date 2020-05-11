225b87a96f1965ec22a644bab31ab588303e4de3
Max Huebler <max@huebler.us>
2020-05-11 16:30:14 -0400

    Cart style changes

--- src/components/Cart/LineItem/index.js
+++ src/components/Cart/LineItem/index.js
@@ -28,22 +28,29 @@ const LineItem = props => {
   }
 
   return (
-    <div className="flex justify-between items-center border-solid border rounded-lg py-4 px-6 mb-4">
-      {variantImage}
-      <p className="text-lg font-bold">
-        {line_item.title}
-        {line_item.variant.title === !'Default Title'
-          ? line_item.variant.title
-          : ''}
-      </p>
-      <p className="font-bold">{selectedOptions}</p>
-      <p>{line_item.quantity}</p>
-      <button
-        className="bg-purple-500 text-white py-2 px-4 hover:bg-purple-600"
-        onClick={handleRemove}
-      >
-        Remove
-      </button>
+    <div className="flex justify-between border-solid border rounded-lg py-4 px-6 mb-6">
+      <div className="flex items-center leading-none">
+        {variantImage}
+        <div className="text-lg px-8">
+          <p className="">{line_item.title}</p>
+          {line_item.variant.title === !'Default Title'
+            ? line_item.variant.title
+            : ''}
+          <br></br>
+          <p className="font-bold">{selectedOptions}</p>
+        </div>
+      </div>
+      <div className="flex items-center">
+        <button className="bg-purple-200 px-4 py-2 rounded-lg mr-8">
+          {line_item.quantity}
+        </button>
+        <button
+          className="bg-purple-500 text-white py-2 px-4 hover:bg-purple-600"
+          onClick={handleRemove}
+        >
+          Remove
+        </button>
+      </div>
     </div>
   )
 }
