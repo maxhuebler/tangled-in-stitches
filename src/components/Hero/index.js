12e80586872f38de817191bb7ee805f01e03546c
Max Huebler <max@huebler.us>
2020-05-19 19:56:57 -0400

    Make some breakpoint adjustments

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -3,9 +3,9 @@ import React from 'react'
 export default function Hero() {
   return (
     <>
-      <div className="flex flex-col bg-purple-200 mx-auto py-24 sm:py-32 sm:rounded-xl max-w-7xl">
+      <div className="flex flex-col bg-purple-200 mx-auto py-24 sm:py-32 xl:rounded-xl max-w-7xl">
         <div className="text-center">
-          <h1 className="text-3xl md:text-5xl uppercase font-bold leading-tight">
+          <h1 className="text-3xl sm:text-5xl uppercase font-bold leading-tight">
             $20 off $100 plus,
           </h1>
           <h1 className="text-xl md:text-3xl uppercase tracking-widest">
