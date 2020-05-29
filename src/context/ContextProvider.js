8bf65afb8f76bc15dbd6562cd134f48945090c23
Max Huebler <max@huebler.us>
2020-05-29 15:26:40 -0400

    Move ContextProvider into context folder

--- src/context/ContextProvider.js
+++ src/context/ContextProvider.js
@@ -32,18 +32,18 @@ const ContextProvider = ({ children }) => {
         ? localStorage.getItem('shopify_checkout_id')
         : null
 
-      const setCheckoutInState = checkout => {
+      const setCheckoutInState = (checkout) => {
         if (isBrowser) {
           localStorage.setItem('shopify_checkout_id', checkout.id)
         }
 
-        updateStore(prevState => {
+        updateStore((prevState) => {
           return { ...prevState, checkout }
         })
       }
 
       const createNewCheckout = () => store.client.checkout.create()
-      const fetchCheckout = id => store.client.checkout.fetch(id)
+      const fetchCheckout = (id) => store.client.checkout.fetch(id)
 
       if (existingCheckoutID) {
         try {
@@ -75,7 +75,7 @@ const ContextProvider = ({ children }) => {
             return
           }
 
-          updateStore(prevState => {
+          updateStore((prevState) => {
             return { ...prevState, adding: true }
           })
 
@@ -88,8 +88,8 @@ const ContextProvider = ({ children }) => {
 
           return client.checkout
             .addLineItems(checkoutId, lineItemsToUpdate)
-            .then(checkout => {
-              updateStore(prevState => {
+            .then((checkout) => {
+              updateStore((prevState) => {
                 return { ...prevState, checkout, adding: false }
               })
             })
@@ -97,8 +97,8 @@ const ContextProvider = ({ children }) => {
         removeLineItem: (client, checkoutID, lineItemID) => {
           return client.checkout
             .removeLineItems(checkoutID, [lineItemID])
-            .then(res => {
-              updateStore(prevState => {
+            .then((res) => {
+              updateStore((prevState) => {
                 return { ...prevState, checkout: res }
               })
             })
@@ -110,8 +110,8 @@ const ContextProvider = ({ children }) => {
 
           return client.checkout
             .updateLineItems(checkoutID, lineItemsToUpdate)
-            .then(res => {
-              updateStore(prevState => {
+            .then((res) => {
+              updateStore((prevState) => {
                 return { ...prevState, checkout: res }
               })
             })
