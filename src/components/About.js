2be7ec35504bd622b6c6b33727a0f64fa5742f47
Max Huebler <max@huebler.us>
2020-06-04 17:19:07 -0400

    Add fluid image to about us page

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import Img from 'gatsby-image'
 import { useStaticQuery, graphql } from 'gatsby'
 
 import Hero from '~/components/Hero'
@@ -12,6 +13,11 @@ export default function About() {
           paragraph1
           paragraph2
           paragraph3
+          image {
+            fluid(maxWidth: 414) {
+              ...GatsbyDatoCmsFluid_tracedSVG
+            }
+          }
         }
       }
     `
@@ -19,17 +25,25 @@ export default function About() {
   return (
     <div className="sm:h-screen">
       <Hero />
-      <div className="mt-12 mx-8">
-        <h1 className="font-bold text-4xl">{about.title}</h1>
-        <p className="text-xl sm:text-justify tracking-wide mt-4">
-          {about.paragraph1}
-        </p>
-        <p className="text-xl sm:text-justify tracking-wide mt-8">
-          {about.paragraph2}
-        </p>
-        <p className="text-xl sm:text-justify tracking-wide mt-8">
-          {about.paragraph3}
-        </p>
+      <div className="mt-8 sm:mt-12 mx-8">
+        <div className="grid grid-flow-row sm:grid-cols-3">
+          <div className="sm:col-span-1 sm:mr-8">
+            <h1 className="sm:hidden font-bold text-4xl mb-4">{about.title}</h1>
+            <Img className="rounded-lg" fluid={about.image.fluid} />
+          </div>
+          <div className="sm:mt-0 sm:col-span-2">
+            <h1 className="hidden sm:flex font-bold text-4xl">{about.title}</h1>
+            <p className="text-xl sm:text-justify tracking-wide mt-4">
+              {about.paragraph1}
+            </p>
+            <p className="text-xl sm:text-justify tracking-wide mt-8">
+              {about.paragraph2}
+            </p>
+            <p className="text-xl sm:text-justify tracking-wide mt-8">
+              {about.paragraph3}
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   )
