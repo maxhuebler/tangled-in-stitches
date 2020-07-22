8183bb21b883e5859106bb9c2454bfc1de2127ef
Max Huebler <max@huebler.us>
2020-07-22 03:22:11 -0400

    Show coming soon screen on all pages

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -20,10 +20,22 @@ const Layout = ({ children }) => {
           }
         `}
         render={(data) => (
-          <div className="max-w-7xl mx-auto">
-            <Navigation siteTitle={data.site.siteMetadata.title} />
-            {children}
-            <Footer siteTitle={data.site.siteMetadata.title} />
+          // <div className="max-w-7xl mx-auto">
+          //   <Navigation siteTitle={data.site.siteMetadata.title} />
+          //   {children}
+          //   <Footer siteTitle={data.site.siteMetadata.title} />
+          // </div>
+          <div className="flex justify-center h-screen bg-purple-200">
+            <div className="flex items-center text-center">
+              <div className="items-center">
+                <h1 className="font-display text-5xl sm:text-7xl leading-none">
+                  Tangled in Stitches
+                </h1>
+                <h2 className="text-xl sm:text-3xl text-white uppercase tracking-widest">
+                  Opening Summer 2020
+                </h2>
+              </div>
+            </div>
           </div>
         )}
       />
