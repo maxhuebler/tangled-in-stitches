3f081a6d10ee672074dc71a9ce44e740f5c76070
Max Huebler <max@huebler.us>
2020-08-18 17:28:55 -0400

    style(Trending): Add separate mobile and desktop queries

--- src/components/Trending/index.tsx
+++ src/components/Trending/index.tsx
@@ -26,10 +26,39 @@ interface ProductProperties {
 }
 
 export default function Trending(): JSX.Element {
-  const { allShopifyProduct } = useStaticQuery(
+  const { desktop, mobile } = useStaticQuery(
     graphql`
       query {
-        allShopifyProduct(limit: 4, sort: { fields: [createdAt], order: ASC }) {
+        desktop: allShopifyProduct(
+          limit: 5
+          sort: { fields: [createdAt], order: ASC }
+        ) {
+          edges {
+            node {
+              id
+              title
+              handle
+              createdAt
+              images {
+                localFile {
+                  childImageSharp {
+                    fluid(maxWidth: 302) {
+                      ...GatsbyImageSharpFluid_withWebp_tracedSVG
+                    }
+                  }
+                }
+              }
+              variants {
+                price
+                compareAtPrice
+              }
+            }
+          }
+        }
+        mobile: allShopifyProduct(
+          limit: 4
+          sort: { fields: [createdAt], order: ASC }
+        ) {
           edges {
             node {
               id
@@ -61,8 +90,37 @@ export default function Trending(): JSX.Element {
       <h1 className="text-2xl text-gray-700 text-center sm:text-left font-bold tracking-widest py-4 uppercase mx-4 sm:mx-8">
         Trending Products
       </h1>
-      <div className="grid grid-cols-2 md:grid-cols-4 gap-4 sm:gap-12 mx-4 sm:mx-8">
-        {allShopifyProduct.edges.map(({ node }: ProductProperties) => (
+      <div className="hidden sm:grid sm:grid-cols-5 sm:gap-12 sm:mx-8">
+        {desktop.edges.map(({ node }: ProductProperties) => (
+          <div className="flex flex-col min-h-full relative " key={node.id}>
+            <Link to={`/product/${node.handle}/`}>
+              <>
+                {node.variants[0].compareAtPrice !== null ? (
+                  <div className="bg-blue-300 px-3 py-1 sm:px-6 sm:py-2 sm:ml-4 sm:mt-4 absolute z-50 shadow-md hover:bg-purple-300">
+                    <h2 className="sm:text-lg tracking-widest text-white uppercase">
+                      sale
+                    </h2>
+                  </div>
+                ) : null}
+                <Image
+                  className="transition-all duration-200 ease-out transform hover:scale-105 h-full w-full rounded-lg hover:opacity-75"
+                  fluid={node.images[0].localFile.childImageSharp.fluid}
+                  alt={node.handle}
+                />
+              </>
+            </Link>
+            <h1 className="text-lg text-center sm:text-left font-bold mt-2">
+              {node.title}
+            </h1>
+            <h2 className="text-center sm:text-left text-gray-700">
+              {node.variants[0].price}
+              <span className="font-bold text-xs"> USD</span>
+            </h2>
+          </div>
+        ))}
+      </div>
+      <div className="grid sm:hidden grid-cols-2 gap-4 mx-4">
+        {mobile.edges.map(({ node }: ProductProperties) => (
           <div className="flex flex-col min-h-full relative " key={node.id}>
             <Link to={`/product/${node.handle}/`}>
               <>
