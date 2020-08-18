629d320f207ce440045ab0556bc366851e86a569
Max Huebler <max@huebler.us>
2020-08-18 02:21:44 -0400

    style: Only display collections when they have products

--- src/components/Collections/Grid/index.tsx
+++ src/components/Collections/Grid/index.tsx
@@ -5,6 +5,7 @@ interface GridProperties {
   node: {
     handle: string
     title: string
+    products: string[]
   }
 }
 
@@ -12,11 +13,14 @@ export default function Grid(): JSX.Element {
   const { allShopifyCollection } = useStaticQuery(
     graphql`
       query {
-        allShopifyCollection(skip: 4) {
+        allShopifyCollection {
           edges {
             node {
               title
               handle
+              products {
+                title
+              }
             }
           }
         }
@@ -24,22 +28,22 @@ export default function Grid(): JSX.Element {
     `
   )
   return (
-    <div className="grid grid-cols-1 md:grid-cols-2 mt-8 gap-4 sm:gap-8 items-center">
-      {allShopifyCollection.edges
-        ? allShopifyCollection.edges.map(({ node }: GridProperties) => (
-            <Link key={node.title.toString()} to={`/collection/${node.handle}`}>
-              <div
-                className="bg-purple-200 mx-4 sm:mx-0 py-32 sm:py-56 text-center rounded-lg font-bold text-3xl uppercase text-white tracking-widest hover:opacity-75 hover:shadow-lg cursor-pointer"
-                style={{
-                  backgroundImage: `linear-gradient(${
-                    Math.random() * 270
-                  }deg, rgba(222,188,255,1) 0%, rgba(169,211,255,1) 100%)`,
-                }}>
-                <h1>{node.title}</h1>
-              </div>
-            </Link>
-          ))
-        : null}
+    <div className="grid grid-cols-1 md:grid-cols-2 gap-4 sm:gap-8 items-center mt-6 sm:mt-0">
+      {allShopifyCollection.edges.map(({ node }: GridProperties) =>
+        node.products.length > 0 ? (
+          <Link key={node.title.toString()} to={`/collection/${node.handle}`}>
+            <div
+              className="bg-purple-200 mx-4 sm:mx-0 py-32 sm:py-56 text-center rounded-lg font-bold text-3xl uppercase text-white tracking-widest opacity-75 hover:opacity-50 cursor-pointer"
+              style={{
+                backgroundImage: `linear-gradient(${
+                  Math.random() * 270
+                }deg, #13547a 0%, #80d0c7 100%`,
+              }}>
+              <h1>{node.title}</h1>
+            </div>
+          </Link>
+        ) : null
+      )}
     </div>
   )
 }
